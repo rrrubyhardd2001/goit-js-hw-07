3be429660f054801febda6767c52205c13421f20
rubyhardd <162762668+rrrubyhardd2001@users.noreply.github.com>
2024-09-07 14:41:35 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,10 +1,10 @@
 const formIdle = document.querySelector(".login-form");
 const onFormSubm = (event) => {
   event.preventDefault();
-  const email = formIdle.elements.email.value;
-  const password = formIdle.elements.password.value;
+  const email = formIdle.elements.email.value.trim();
+  const password = formIdle.elements.password.value.trim();
 
-  if (!email || !password === 0) {
+  if (!email || !password) {
     alert("All form fields must be filled in");
     return;
   }
